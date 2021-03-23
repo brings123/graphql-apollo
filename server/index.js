09aa14b4d522da0b842a74ba89e85f34233f6660
Chakib Afraoucene <chakib.afraoucene@legal-doctrine.com>
2021-03-23 16:55:52 +0100

    :hammer: update graph (author added + relationship)

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
 
 require("dotenv").config();
 
 const app = express();
 
-app.use("/graphql", graphqlHTTP({}));
+app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
 
 const PORT = process.env.PORT || 4000;
 
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,11 @@
 const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLString } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt,
+} = graphql;
 
 const _ = require("lodash");
 
@@ -8,16 +14,37 @@ var books = [
     id: "1",
     title: "The Secret",
     gender: "Personal development",
+    authorId: "2",
   },
   {
     id: "2",
     title: "12 Rules for life",
     gender: "Socialogy",
+    authorId: "1",
   },
   {
     id: "3",
     title: "The Subtle Art Of Not Giving A Fuck!",
     gender: "Personal development",
+    authorId: "3",
+  },
+];
+
+var authors = [
+  {
+    id: "1",
+    name: "Jordan B. Peterson",
+    age: 55,
+  },
+  {
+    id: "2",
+    name: "Rhonda Byrne",
+    age: 65,
+  },
+  {
+    id: "3",
+    name: "Mark Manson",
+    age: 33,
   },
 ];
 
@@ -27,6 +54,21 @@ const BookType = new GraphQLObjectType({
     id: { type: GraphQLString },
     title: { type: GraphQLString },
     gender: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        return _.find(authors, { id: parent.authorId });
+      },
+    },
+  }),
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
   }),
 });
 
@@ -35,15 +77,24 @@ const RootQuery = new GraphQLObjectType({
   fields: () => ({
     book: {
       type: BookType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         // what should be returned by this query
         return _.find(books, { id: args.id });
       },
     },
+
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        // what should be returned by this query
+        return _.find(authors, { id: args.id });
+      },
+    },
   }),
 });
 
-module.exports = new GraphQLObjectType({
+module.exports = new GraphQLSchema({
   query: RootQuery,
 });
