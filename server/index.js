294bac15ea98577e21017482a2efa179e27fbc7c
Chakib Afraoucene <chakib.afraoucene@legal-doctrine.com>
2021-03-23 14:03:39 +0100

    :hammer: graphql initialization

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+
 require("dotenv").config();
 
-const app = new express();
+const app = express();
+
+app.use("/graphql", graphqlHTTP({}));
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, `Express server listening on port ${PORT}`);
+app.listen(PORT, () => console.log(`Express server listening on port ${PORT}`));
